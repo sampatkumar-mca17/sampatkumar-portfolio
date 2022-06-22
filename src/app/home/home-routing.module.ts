33dd52a3c1732b44adf4f87f354e62a096c309ae
samupa <sampatkumar.upase@syncron.com>
2022-06-23 02:47:39 +0530

    changes to index

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -11,6 +11,11 @@ const routes: Routes = [
       {
         path:'view',
         component:ViewComponent
+      },
+      {
+        path:'',
+        redirectTo:'view',
+        pathMatch:'full'
       }
     ]
   },
